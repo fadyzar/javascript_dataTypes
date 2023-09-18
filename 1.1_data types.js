853fe5f009549c4d327efd4872c93624645f0972
fadyZar <93481957+fadyzar@users.noreply.github.com>
2023-09-18 12:54:09 +0300

    Add files via upload

---
+++ 1.1_data types.js
@@ -0,0 +1,33 @@
+// //1. Create a variable whose value holds a string.
+// 2. Create a variable whose value holds a number.
+// 3. Create a variable whose value holds an object.
+// 4. Create a variable whose value holds a boolean.
+// 5. Create a variable whose value holds null.
+// 6. Create a variable whose value is undefined.
+// 7. Create a variable whose value holds a symbol.
+
+const myName = 'fady';
+console.log(typeof myName);
+
+const Num = 5 ; 
+console.log(typeof Num);
+
+let empty = null;
+console.log(typeof empty);
+
+
+
+const boolean = true;
+console.log(typeof boolean);
+
+const Number = null;
+console.log(typeof Number);
+
+let str  = "lol"[4];
+console.log(typeof str);
+
+const sym2 = Symbol("foo");
+console.log(typeof sym2);
+
+
+
---
+++ 1.2_more_on_primitive_data_types.js
@@ -0,0 +1,72 @@
+/*--Delete the wrong answers--
+1. Which of the following is/are strings?
+
+c) '4'
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+
+d) -4
+3. Which of the following is/are booleans?
+a) true
+b) false
+
+4. What is the result of 80 + 71.2?
+a) 151.2
+
+5. What is the result of "80" + 71.2?
+
+c) "8071.2"
+6. Does 100 + 30 produce a number or a string?
+a) number
+
+7. Does "100" + 30 produce a number or a string?
+
+b)string
+*/
+
+//------Submit your answers under the
+// question//
+//create three different ways to declare variables
+var fady = 3;
+let numberOf = 40;
+const StringOf = 'Messi';
+
+//declare a variable and reassign a value to it
+let Num;
+Num = 5;
+
+//create a variable and after that assign a string
+// with its value being: He's got it!/*
+let word ;
+word = "He's got it!"; 
+
+// 1. create a variable and assign a value on how
+// much a restaurant bill is.
+let  bill = 100;
+console.log(`The restaurant bill is ${bill}$`);
+
+
+
+// 2. create a variable and assign a value on how
+// much tax they should pay.
+
+let  tax= 300;
+console.log(`You should pay ${tax} $ for tax`);
+
+
+
+// 3. create a variable that will calculate the bill
+// * tax and its output would be: Your total bill is
+// <total bill> $.*/
+let  tax= 300;
+let bill = 240;
+let calc = tax * bill ;
+console.log(`Your total bill is ${calc}$`);
+
+
+// Round the number 50.6 to its nearest integer
+let x = Math.round(5.6);
+console.log(x);
+//Create a variable that is undefined
+let str  = "lol"[4];

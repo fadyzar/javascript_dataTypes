2c81cd444c2b6ab6a68b3bc87da4006ededcc084
fadyZar <93481957+fadyzar@users.noreply.github.com>
2023-09-19 16:19:06 +0300

    Add files via upload

---
+++ 9.3_for.js
@@ -0,0 +1,12 @@
+const food1 = ["Noodle", "Pasta", "Ice-cream", "Meat", "Cucumber",
+"Olives"];
+const food2 = ["Fries", "Ice-cream", "Pizza", "Olives", "Hamburgers"];
+
+
+for (let i = 0; i < food1.length; i++) {
+    if (food2.includes(food1[i])) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
